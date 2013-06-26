Mikulas/Skeleton21

added Orm configuration and example entities

app/model/orm/Users/UsersMapper.php
@@ -1,0 +1,12 @@
+<?php
+
+namespace App;
+
+
+/**
+ * Maps User entities to database.
+ */
+class UsersMapper extends DibiMapper
+{
+
+}

Clevis/ClevisArticles

app/model/orm/Users/UsersMapper.php
@@ -1,0 +1,12 @@
+<?php
+
+namespace App;
+
+
+/**
+ * Maps User entities to database.
+ */
+class UsersMapper extends Mapper
+{
+
+}