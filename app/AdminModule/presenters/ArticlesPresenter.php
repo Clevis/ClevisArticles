2d0fac8eb9dbef1b9d3ca8e33d2fce067487d54b
Tomas Susanka <tsusanka@gmail.com>
2013-07-18 14:06:42 +0200

    Admin/Articles: editting

--- app/AdminModule/presenters/ArticlesPresenter.php
+++ app/AdminModule/presenters/ArticlesPresenter.php
@@ -2,6 +2,9 @@
 
 namespace App\Admin;
 
+use Nette\Application\UI\Form,
+	App\Article;
+
 
 /**
  * Articles editting presenter
@@ -9,5 +12,76 @@ namespace App\Admin;
 final class ArticlesPresenter extends BasePresenter
 {
 
+	/**
+	 * Adding an article
+	 */
+	public function actionAdd()
+	{
+		$this->template->heading = 'Přidat článek';
+		$this->view = 'edit';
+	}
+
+	/**
+	 * Article editting
+	 * @param  int
+	 */
+	public function actionEdit($id)
+	{
+		$article = $this->orm->articles->getById($id);
+		$this->template->heading = 'Article editting';
+
+		$this['editArticle']->setDefaults($article);
+	}
+
+	/**
+	 * Form for editting and adding articles
+	 * @return Nette\Form
+	 */
+	protected function createComponentEditArticle()
+	{
+		$form = new Form;
+
+		$form->addHidden('id');
+
+		$form->addText('title', 'Titulek')
+			->setRequired('Prosím vyplňte titulek.');
+		$form->addTextArea('intro', 'Perex');
+		$form->addTextArea('text', 'text');
+
+		$form->addCheckbox('visible', 'Viditelné');
+
+		$form->addSubmit('save', 'Uložit');
+
+		$form->onSuccess[] = callback($this, 'editArticleFormSubmitted');
+
+		return $form;
+	}
+
+	/**
+	 * Form processing
+	 */
+	public function editArticleFormSubmitted(Form $form)
+	{
+		$values = $form->getValues();
+
+		if (empty($values->id))
+		{
+			$article = new Article();
+			$this->orm->articles->attach($article);
+			$article->setValues($form->values);
+		}
+		else
+		{
+			$article = $this->orm->articles->getById($values->id);
+			$article->setValues($values);
+		}
+
+		$article->createdBy = $this->user->id;
+
+		$this->orm->articles->flush();
+
+		$this->flashMessage('Článek byl úspěšně uložen.', 'success');
+		$this->redirect('Articles:');
+	}
 
 }
--- migrations/struct/2013-07-18-articles.sql
+++ migrations/struct/2013-07-18-articles.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `articles` (
   `id` int(1) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL COMMENT 'title of the article',
-  `intro` varchar(255) NOT NULL COMMENT 'short intro about the article',
+  `intro` text NOT NULL COMMENT 'short intro about the article',
   `text` text NOT NULL COMMENT 'main text',
   `created_at` datetime NOT NULL COMMENT 'when was the article created',
   `created_by_id` int(1) NOT NULL COMMENT 'by whom was the article created',
